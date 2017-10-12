95eab54e4821cce0d6c53d3c01b003ffbbab5527
Dan Timoney <dtimoney@att.com>
2017-10-12 16:01:06 -0400

    Fixed vnfapi install issue
    Fixed issues with vnfapi install:
     - Resolved issue installing vnftools by converting to blueprint
     - Resolved issue not finding RPC implementation of vnfapi by fixing
       dependency feature vnftools, and by converting vnfapi to blueprint.
    
    The root cause in both cases was probably something wrong with the
    provider configuration, but it was simpler (not to mention more
    maintainable) to just convert from config to blueprint.
    
    Change-Id: I7010a845687673c932457774f3103834d328ab84
    Issue-ID: SDNC-119
    Signed-off-by: Dan Timoney <dtimoney@att.com>

--- vnfapi/provider/src/main/java/org/onap/sdnc/vnfapi/VNFSDNSvcLogicServiceClient.java
+++ vnfapi/provider/src/main/java/org/onap/sdnc/vnfapi/VNFSDNSvcLogicServiceClient.java
@@ -8,9 +8,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,24 +43,12 @@ public class VNFSDNSvcLogicServiceClient {
     private static final Logger LOG = LoggerFactory
             .getLogger(VNFSDNSvcLogicServiceClient.class);
 
-    private SvcLogicService svcLogic = null;
+    private final SvcLogicService svcLogic;
 
-    public VNFSDNSvcLogicServiceClient()
+    public VNFSDNSvcLogicServiceClient(final SvcLogicService svcLogicService)
     {
-        BundleContext bctx = FrameworkUtil.getBundle(SvcLogicService.class).getBundleContext();
+    		this.svcLogic = svcLogicService;
 
-        // Get SvcLogicService reference
-        ServiceReference sref = bctx.getServiceReference(SvcLogicService.NAME);
-        if (sref  != null)
-        {
-            svcLogic =  (SvcLogicService) bctx.getService(sref);
-
-        }
-        else
-        {
-            LOG.warn("Cannot find service reference for "+SvcLogicService.NAME);
-
-        }
     }
 
     public boolean hasGraph(String module, String rpc, String version, String mode) throws SvcLogicException
