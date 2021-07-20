605fa6cedd43482cb5ca871a8b54f59683f5d560
Jonathan Platt <jonathan.platt@att.com>
2021-07-20 10:13:48 -0400

    Use host name verifier that accepts IP addresses
    Modify 'SdncOdlConnection.java' to use a hostname verfier that
    handles IP addresses as special cases, so that they can be safely
    ignored in lab environments
    
    Issue-ID: SDNC-1497
    Signed-off-by: Jonathan Platt <jonathan.platt@att.com>
    Change-Id: I73ca672226b055eb7d6896ec260777261f9bb455

--- optical-service/provider/src/main/java/org/onap/sdnc/northbound/asyncrequests/SdncOdlConnection.java
+++ optical-service/provider/src/main/java/org/onap/sdnc/northbound/asyncrequests/SdncOdlConnection.java
@@ -32,6 +32,7 @@ import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSession;
 import org.apache.commons.codec.binary.Base64;
+import org.onap.ccsdk.sli.core.utils.common.AcceptIpAddressHostNameVerifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -105,12 +106,7 @@ public class SdncOdlConnection {
         httpConn.setUseCaches(false);
 
         if (httpConn instanceof HttpsURLConnection) {
-            HostnameVerifier hostnameVerifier = new HostnameVerifier() {
-                @Override
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                }
-            };
+        	HostnameVerifier hostnameVerifier = new AcceptIpAddressHostNameVerifier();
             ((HttpsURLConnection) httpConn).setHostnameVerifier(hostnameVerifier);
         }
 
